b29c839e2265e6c6cb57a7d6e86abef76f43f935
Yuichi Takada <takadyuichi@gmail.com>
2017-07-16 22:44:05 +0900

    Display sort algorithm on table

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import BubbleSort from './BubbleSort';
 import InsertionSort from './InsertionSort';
+import SelectionSort from './SelectionSort';
 
 class App extends Component {
   render() {
@@ -12,8 +13,21 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Sort Algorithm</h2>
         </div>
-        <BubbleSort defaultArray={[8, 20, 19, 14, 13, 6, 9, 5, 7, 16, 2, 4, 3, 10, 1, 17, 11, 18, 15, 12]} />
-        <InsertionSort defaultArray={[8, 20, 19, 14, 13, 6, 9, 5, 7, 16, 2, 4, 3, 10, 1, 17, 11, 18, 15, 12]} />
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <BubbleSort defaultArray={[8, 20, 19, 14, 13, 6, 9, 5, 7, 16, 2, 4, 3, 10, 1, 17, 11, 18, 15, 12]} />
+              </td>
+              <td>
+                <SelectionSort defaultArray={[8, 20, 19, 14, 13, 6, 9, 5, 7, 16, 2, 4, 3, 10, 1, 17, 11, 18, 15, 12]} />
+              </td>
+              <td>
+                <InsertionSort defaultArray={[8, 20, 19, 14, 13, 6, 9, 5, 7, 16, 2, 4, 3, 10, 1, 17, 11, 18, 15, 12]} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
