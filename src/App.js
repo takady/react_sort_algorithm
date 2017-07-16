74027caa91725de469deb3b22389047ec4ba292b
Yuichi Takada <takadyuichi@gmail.com>
2017-07-16 18:59:06 +0900

    InsertionSort

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import BubbleSort from './BubbleSort.js';
+import BubbleSort from './BubbleSort';
+import InsertionSort from './InsertionSort';
 
 class App extends Component {
   render() {
-    const defaultArray = [8, 20, 19, 14, 13, 6, 9, 5, 7, 16, 2, 4, 3, 10, 1, 17, 11, 18, 15, 12]
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Sort Algorithm</h2>
         </div>
-        <BubbleSort defaultArray={defaultArray} />
+        <BubbleSort defaultArray={[8, 20, 19, 14, 13, 6, 9, 5, 7, 16, 2, 4, 3, 10, 1, 17, 11, 18, 15, 12]} />
+        <InsertionSort defaultArray={[8, 20, 19, 14, 13, 6, 9, 5, 7, 16, 2, 4, 3, 10, 1, 17, 11, 18, 15, 12]} />
       </div>
     );
   }
---
+++ src/InsertionSort.js
@@ -0,0 +1,51 @@
+import classNames from 'classnames';
+import React, { Component } from 'react';
+import './App.css';
+
+class InsertionSort extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {cursor: 1, sortedIndex: 1, step: 0, array: props.defaultArray};
+  }
+
+  componentDidMount() {
+    setInterval(() => {this.sort(this.state.array)}, 70);
+  }
+
+  sort(array) {
+    if(this.state.sortedIndex > this.state.array.length)
+      return;
+
+    if(array[this.state.cursor - 1] > array[this.state.cursor]) {
+      const swap = array[this.state.cursor - 1];
+      array[this.state.cursor - 1] = array[this.state.cursor];
+      array[this.state.cursor] = swap;
+      this.setState({array: array});
+    } else {
+      this.setState({cursor: this.state.sortedIndex + 1, sortedIndex: this.state.sortedIndex + 1})
+    }
+
+    console.debug(`cursor: ${this.state.cursor}`);
+    if(this.state.cursor === 0) {
+      this.setState({cursor: this.state.sortedIndex + 1, sortedIndex: this.state.sortedIndex + 1})
+    } else {
+      this.setState({cursor: this.state.cursor - 1});
+    }
+    this.setState({step: this.state.step + 1});
+  }
+
+  render() {
+    return (
+      <div className="Sort InsertionSort">
+        <dl className="BarChart">
+          {this.state.array.map((x, i) => {
+            return <dd className={classNames('Bar', {current: i === this.state.cursor})} key={i} style={{height: x * 10}} />;
+          })}
+        </dl>
+        <p>{this.state.step}</p>
+      </div>
+    );
+  }
+}
+
+export default InsertionSort;
