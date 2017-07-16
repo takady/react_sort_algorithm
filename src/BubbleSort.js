b8c972ea0f11885ecbd1a16605a23ca63d7ddf2d
Yuichi Takada <takadyuichi@gmail.com>
2017-07-16 22:43:37 +0900

    Tweak

--- src/BubbleSort.js
+++ src/BubbleSort.js
@@ -9,10 +9,10 @@ class BubbleSort extends Component {
   }
 
   componentDidMount() {
-    setInterval(() => {this.bubbleSort(this.state.array)}, 70);
+    setInterval(() => {this.sort(this.state.array)}, 70);
   }
 
-  bubbleSort(array) {
+  sort(array) {
     if(this.state.sortedIndex >= this.state.array.length)
       return;
 
@@ -23,26 +23,26 @@ class BubbleSort extends Component {
         array[this.state.cursor] = swap;
       }
 
-      console.debug(`sortedIndex: ${this.state.sortedIndex}, cursor: ${this.state.cursor}`);
-
       if(this.state.cursor - 1 === this.state.sortedIndex) {
         this.setState({cursor: this.state.array.length, sortedIndex: this.state.sortedIndex + 1});
       } else {
         this.setState({cursor: this.state.cursor - 1, array: array});
       }
-      this.setState({step: this.state.step + 1});
     }
+
+    this.setState({step: this.state.step + 1});
   }
 
   render() {
     return (
       <div className="Sort BubbleSort">
+        <h2>BubbleSort</h2>
         <dl className="BarChart">
           {this.state.array.map((x, i) => {
             return <dd className={classNames('Bar', {current: i === this.state.cursor})} key={i} style={{height: x * 10}} />;
           })}
         </dl>
-        <p>{this.state.step}</p>
+        <p>Step: {this.state.step}</p>
       </div>
     );
   }
