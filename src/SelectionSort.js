5b059e072b91e8af6618690b10246a156b7219f1
Yuichi Takada <takadyuichi@gmail.com>
2017-07-17 23:24:31 +0900

    Refactor

--- src/SelectionSort.js
+++ src/SelectionSort.js
@@ -1,47 +1,64 @@
-import classNames from 'classnames';
+import _ from 'lodash';
 import React, { Component } from 'react';
 import './App.css';
 
 class SelectionSort extends Component {
   constructor(props) {
     super(props);
-    this.state = {cursor: 1, sortedIndex: 0, minIndex: 0, step: 0, array: props.defaultArray};
+    this.state = {step: 0, array: props.defaultArray};
+    this.renderQueue = [];
+    this.running = false;
+  }
+
+  processRenderQueue() {
+    if (this.renderQueue.length > 0) {
+      let array = this.renderQueue.shift();
+      this.setState({step: this.state.step + 1, array: array});
+      this.running = true;
+    } else {
+      this.running = false;
+    }
   }
 
   componentDidMount() {
-    setInterval(() => {this.sort(this.state.array)}, 70);
+    this.sort(this.state.array, (array) => {
+      let cloned = _.clone(array);
+      this.renderQueue.push(cloned);
+
+      if (!this.running) {
+        this.processRenderQueue();
+      }
+    });
   }
 
-  sort(array) {
-    if(this.state.sortedIndex >= this.state.array.length - 1)
-      return;
+  componentDidUpdate() {
+    _.delay(this.processRenderQueue.bind(this), 30);
+  }
 
-    if(this.state.cursor === array.length) {
-      const swap = array[this.state.sortedIndex];
-      array[this.state.sortedIndex] = array[this.state.minIndex];
-      array[this.state.minIndex] = swap;
-      this.setState({
-        sortedIndex: this.state.sortedIndex + 1,
-        cursor: this.state.sortedIndex + 2,
-        minIndex: this.state.sortedIndex + 1,
-        array: array
-      });
-    } else {
-      if(array[this.state.cursor] < array[this.state.minIndex]) {
-        this.setState({minIndex: this.state.cursor});
+  sort(array, callback) {
+    for(let i = 0; i < array.length - 1; i++) {
+      let minIndex = i;
+
+      for(let j = i + 1; j < array.length; j++) {
+        if(array[j] < array[minIndex]) {
+          minIndex = j;
+        }
       }
-      this.setState({cursor: this.state.cursor + 1});
+
+      const swap = array[i];
+      array[i] = array[minIndex];
+      array[minIndex] = swap;
+      callback(array);
     }
-    this.setState({step: this.state.step + 1});
   }
 
   render() {
     return (
       <div className="Sort SelectionSort">
-        <h2>SelectionSort</h2>
+        <h2>Selection</h2>
         <dl className="BarChart">
           {this.state.array.map((x, i) => {
-            return <dd className={classNames('Bar', {current: i === this.state.cursor})} key={i} style={{height: x * 10}} />;
+            return <dd className="Bar" key={i} style={{height: x * 10}} />;
           })}
         </dl>
         <p>Step: {this.state.step}</p>
